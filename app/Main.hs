73465f79a52beaf1eaf9837a6dab48be625c03f9
Maksim Golubev <maksim.golubev72@gmail.com>
2021-08-03 07:35:21 +0300

    Some options added

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ import System.IO (IOMode (ReadMode), stdin, stdout, openFile, hGetContents, hSet
 import System.IO.Temp (withSystemTempFile)
 import System.Directory (copyFile)
 import Control.Monad (when)
-import Data.Maybe (isJust, fromJust)
+import Data.Maybe (isJust, fromJust, fromMaybe)
 
 import Paths_csv2xml (version)
 import Data.Version (showVersion)
@@ -22,6 +22,11 @@ data Flags =
     Flags
     { input_encoding :: Maybe EncodingName
     , output_encoding :: Maybe EncodingName
+    , record_element_name :: Maybe String
+    , field_element_name :: Maybe String
+    , is_indexed_field_names :: Bool
+    , xml_namespace :: Maybe String
+    , tab_delimited :: Bool
     , file_names :: [FilePath]
     } deriving (Data, Typeable)
 
@@ -37,6 +42,29 @@ optsDefinition = getProgName >>= \programName -> return $
                    &= help "Encoding of resulting XML file. If not specified then UTF-8 will be used as default"
                    &= explicit &= name "e" &= name "o" &= name "output-encoding"
                    &= typ "ENC"
+             , is_indexed_field_names =
+                 def
+                 &= help "By default all field elements are created with the same name. Use this option to add index number suffix to the field names"
+                 &= explicit &= name "x" &= name "indexed-fields"
+             , record_element_name =
+                 def
+                 &= help "Name for the record XML elements. If not specified the name of `r` will be used"
+                 &= explicit &= name "r" &= name "record-tag-name"
+                 &= typ "TAG_NAME"
+             , field_element_name =
+                 def
+                 &= help "Name for the field XML elements. If not specified the name of `f` will be used"
+                 &= explicit &= name "f" &= name "field-tag-name"
+                 &= typ "TAG_NAME"
+             , xml_namespace =
+                 def
+                 &= help "XML namespace for all elements of the output XML document. By default no namespace is used."
+                 &= explicit &= name "n" &= name "namespace"
+                 &= typ "XML_NAME_SPACE"
+             , tab_delimited =
+                 def
+                 &= help "Specify this option if the fields in the input CSV file are separated by a tab character. By default, the fields are separated by a comma."
+                 &= explicit &= name "t" &= name "tab-delimited"
              , file_names =
                  def &= args &= typ "INPUT_FILE [OUTPUT_FILE]"
              }
@@ -67,19 +95,33 @@ main = do
     inputEncoding = input_encoding opts
     outputEncoding = output_encoding opts
 
-  (inFileH, sourceName) <-
+  (inFileH, source) <-
       if inFileName == "-"
-          then return (stdin, "stdin")
-          else openFile inFileName ReadMode >>= \h -> return (h, inFileName)
+          then return (stdin, mkSourceFromStdin)
+          else openFile inFileName ReadMode >>= \h -> mkSourceFromFilePath inFileName >>= \s ->  return (h, s)
   hSetBinaryMode inFileH True
   when (isJust inputEncoding) $
        hSetEncoding inFileH =<< mkTextEncoding' (fromJust inputEncoding)
   inFileContent <- hGetContents inFileH
 
+  let
+      convertOptions = flagsToConvertConfig opts source
+
   if outFileName == "-"
       then do
-          processFile sourceName inFileContent stdout outputEncoding
+          convertCsv2Xml convertOptions inFileContent stdout outputEncoding
       else
           withSystemTempFile "csv2xml.xml" $ \tmpFileP tmpFileH -> do
-            processFile sourceName inFileContent tmpFileH outputEncoding
+            convertCsv2Xml convertOptions inFileContent tmpFileH outputEncoding
             copyFile tmpFileP outFileName
+
+flagsToConvertConfig :: Flags -> Source -> ConvertConfig
+flagsToConvertConfig flags source =
+    mkDefaultConvertConfig
+    { isTabDelimited = tab_delimited flags
+    , isIndexedFieldNames = is_indexed_field_names flags
+    , recordElementName = fromMaybe "r" $ record_element_name flags
+    , fieldElementName = fromMaybe "f" $ field_element_name flags
+    , xmlNameSpace = xml_namespace flags
+    , source = Just source
+    }
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,20 +2,71 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE LambdaCase #-}
 module Lib
-    ( processFile
+    ( convertCsv2Xml
     , mkTextEncoding'
     , EncodingName
+    , ConvertConfig (..)
+    , Source
+    , mkSourceFromFilePath
+    , mkSourceFromStdin
+    , mkDefaultConvertConfig
     ) where
 
 import System.IO
 import Control.Monad ( when )
-import Data.Char ( toUpper )
+import Data.Char ( toUpper, ord )
 import qualified Data.ByteString.Lazy.UTF8 as BLU
 import qualified Data.Csv as CSV
 import Data.Vector (Vector)
+import qualified Data.Vector as V
 import Data.Maybe ( fromMaybe )
+import System.Directory (doesFileExist)
+import System.PosixCompat.Files (getFileStatus, modificationTimeHiRes)
+import Data.Time.Clock (UTCTime)
+import Data.Time.LocalTime (ZonedTime, utcToLocalZonedTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import Data.Time.Format (formatTime, defaultTimeLocale, iso8601DateFormat)
+import Control.Monad.Reader (ReaderT, runReaderT, asks, ask)
+import Control.Monad.Identity (Identity, runIdentity)
 
 type EncodingName = String
+
+data Source =
+    Source
+    {
+      name :: String
+    , fileName :: Maybe FilePath
+    , timestamp :: Maybe ZonedTime
+    }
+
+mkSourceFromFilePath :: FilePath -> IO Source
+mkSourceFromFilePath fileName =
+    Source fileName (Just fileName) <$> getFileModificationTime fileName
+
+mkSourceFromStdin :: Source
+mkSourceFromStdin = Source "stdin" Nothing Nothing
+
+data ConvertConfig =
+    ConvertConfig
+    {
+      isTabDelimited :: Bool
+    , isIndexedFieldNames :: Bool
+    , recordElementName :: String
+    , fieldElementName :: String
+    , source :: Maybe Source
+    , xmlNameSpace :: Maybe String
+    }
+
+mkDefaultConvertConfig :: ConvertConfig
+mkDefaultConvertConfig =
+    ConvertConfig
+    { isTabDelimited = False
+    , isIndexedFieldNames = False
+    , recordElementName = "r"
+    , fieldElementName = "f"
+    , source = Nothing
+    , xmlNameSpace = Nothing
+    }
 
 mkTextEncoding' :: EncodingName -> IO TextEncoding
 mkTextEncoding' en =
@@ -43,35 +94,74 @@ type Records = Vector Record
 
 type CsvInput = String
 
-recordElement = "r"
-fieldElement = "f"
-
-processFile :: String -> CsvInput -> Handle -> Maybe EncodingName -> IO ()
-processFile sourceName csvInput outFileH outputEncoding = do
-  case CSV.decode CSV.NoHeader (BLU.fromString csvInput) of
+convertCsv2Xml :: ConvertConfig -> CsvInput -> Handle -> Maybe EncodingName -> IO ()
+convertCsv2Xml convertConfig csvInput outFileH outputEncoding = do
+  case CSV.decodeWith decodeOptions CSV.NoHeader (BLU.fromString csvInput) of
     Left err -> errorWithoutStackTrace err
     Right (r :: Records) -> do
       hSetBinaryMode outFileH True
       hSetEncoding outFileH =<< mkTextEncoding' outputEncoding'
-      hPutStrLn outFileH $ makeDocument sourceName outputEncoding' r
+      hPutStrLn outFileH $ runConverting convertConfig $ makeDocument outputEncoding' r
   where
     outputEncoding' = fromMaybe "utf-8" outputEncoding
+    decodeOptions =
+        let
+            def = CSV.defaultDecodeOptions
+        in
+          if isTabDelimited convertConfig
+              then def { CSV.decDelimiter = fromIntegral (ord '\t') }
+              else def
 
-makeDocument :: String -> String -> Records -> String
-makeDocument sourceName outputEncoding records =
-    "<?xml version=\"1.0\" encoding=\"" <> outputEncoding <> "\"?>"
-    <> "<csvPacket source=\"" <> escapeAttributeData sourceName <> "\">"
-    <> mkRecords records
-    <> "</csvPacket>"
+type Converting a = ReaderT ConvertConfig Identity a
 
-mkRecords :: Records -> String
-mkRecords = concatMap mkRecord
+runConverting :: ConvertConfig -> Converting a -> a
+runConverting convertConfig act =
+    runIdentity $ runReaderT act convertConfig
 
-mkRecord :: Record -> String
-mkRecord = mkElement recordElement . concatMap mkField
+makeDocument :: EncodingName -> Records -> Converting String
+makeDocument outputEncoding records = do
+    cfg <- ask
+    records' <- mkRecords records
+    return $
+      "<?xml version=\"1.0\" encoding=\"" <> outputEncoding <> "\"?>"
+      <> "<csvPacket" <> mkXmlns cfg <> mkSourceAttributes cfg <> ">"
+      <> records'
+      <> "</csvPacket>"
+    where
+      mkSourceAttributes cfg =
+          case source cfg of
+            Just s -> " source=\"" <> (escapeAttributeData . name) s <> "\""
+                      <> case timestamp s of
+                           Just t -> " timestamp=\"" <> (escapeAttributeData . toXsdDateTimeFormat) t <> "\""
+                           Nothing -> ""
+            Nothing -> ""
+      mkXmlns cfg =
+          case xmlNameSpace cfg of
+            Just ns -> " xmlns=\"" <> escapeAttributeData ns <> "\""
+            Nothing -> ""
 
-mkField :: Field -> String
-mkField = mkElement fieldElement . escapeCharacterData
+mkRecords :: Records -> Converting String
+mkRecords rs = concat <$> mapM mkRecord rs
+
+mkRecord :: Record -> Converting String
+mkRecord r =
+  asks (mkElement . recordElementName) <*> mkFields r
+
+mkFields :: Record -> Converting String
+mkFields fs = do
+    isIndexed <- asks isIndexedFieldNames
+    fields <- if isIndexed
+                  then mapM mkIndexedField (V.zip (V.fromList [1..]) fs)
+                  else mapM mkField fs
+    return $ concat fields
+
+mkField :: Field -> Converting String
+mkField f =
+  asks (mkElement . fieldElementName) <*>  pure (escapeCharacterData f)
+
+mkIndexedField :: (Integer, Field) -> Converting String
+mkIndexedField (i, f) =
+  asks (mkElement . (<> show i) . fieldElementName) <*> pure (escapeCharacterData f)
 
 mkElement :: String -> String -> String
 mkElement elementName elementContent =
@@ -90,3 +180,20 @@ escapeAttributeData = escapeAttributeData' . escapeCharacterData
     escapeAttributeData' = concatMap $ \case
         '"' -> "&quot;"
         c   -> [c]
+
+getFileModificationTime :: FilePath -> IO (Maybe ZonedTime)
+getFileModificationTime fileName = do
+    isExists <- doesFileExist fileName
+    if isExists
+        then
+            Just <$> (utcToLocalZonedTime . posixSecondsToUTCTime . modificationTimeHiRes =<< getFileStatus fileName)
+        else
+            return Nothing
+
+toXsdDateTimeFormat :: ZonedTime -> String
+toXsdDateTimeFormat t =
+  let
+      s = formatTime defaultTimeLocale (iso8601DateFormat $ Just "%H:%M:%S%Q%z") t
+      (prefix, suffix) = splitAt (length s -2) s
+  in
+    concat [prefix, ":", suffix]
